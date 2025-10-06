19c9eb9fd4dcc6e830a3734befe5dce013e1f689
Miguel Rojas <miguel.rojas1991@gmail.com>
2025-10-06 15:46:59 -0500

    fix(db): set default DNS result order to prioritize IPv4

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,8 @@
+import dns from "dns";
 import pkg from "pg";
 const { Pool } = pkg;
+
+dns.setDefaultResultOrder("ipv4first"); // 👈 fuerza IPv4
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
